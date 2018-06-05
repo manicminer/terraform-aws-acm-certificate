4b01d77a725197fe9818130428cd4fa5436be89c
Tom Bamford <tom@atplanet.co.za>
2018-06-05 22:45:04 +0200

    Add variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "arn" {
+  description = "The ARN of the issued certificate"
   value = "${aws_acm_certificate.main.arn}"
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
-variable "domain_names" { type = "list" }
-variable "zone_id" {}
+variable "domain_names" {
+  description = "List of domains to associate with the new certificate. ACM currently supports up to 10 domains, any or all of which can contain wildcards. The first domain should be the primary domain"
+  type = "list"
+}
+
+variable "zone_id" {
+  description = "The Route53 zone ID in which to create validation records"
+}
